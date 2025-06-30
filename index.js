2c9ffee805dfc4f4fed13584016a63bb655ad4d7
Eugene Gabriel <103780583+CodeWithEugene@users.noreply.github.com>
2025-06-30 19:06:36 +0300

    Update commit count from 5000 to 10000 in the makeCommits function

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const makeCommits = (n) => {
   const dayOffset = random.int(0, totalDays);
   const commitDate = startDate.clone().add(dayOffset, "days").format();
 
-  console.log(`Commit ${5000 - n + 1}/5000 - Date: ${commitDate}`);
+  console.log(`Commit ${10000 - n + 1}/10000 - Date: ${commitDate}`);
 
   const data = { date: commitDate };
   
@@ -43,4 +43,4 @@ const makeCommits = (n) => {
 };
 
 // Start the commit process with 1000 commits
-makeCommits(5000);
+makeCommits(10000);
