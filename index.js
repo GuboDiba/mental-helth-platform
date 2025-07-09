b1599708d225a3f7c369f9c36345cf7e463a1df5
CodeWithEugene <eugenegabriel.ke@gmail.com>
2025-07-09 13:44:57 +0300

    fix: correct commit count in logging and initialization for makeCommits function

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const makeCommits = (n) => {
   const dayOffset = random.int(0, totalDays);
   const commitDate = startDate.clone().add(dayOffset, "days").format();
 
-  console.log(`Commit ${5000 - n + 1}/5000 - Date: ${commitDate}`);
+  console.log(`Commit ${1000 - n + 1}/1000 - Date: ${commitDate}`);
 
   const data = { date: commitDate };
   
@@ -42,5 +42,5 @@ const makeCommits = (n) => {
   });
 };
 
-// Start the commit process with 5000 commits
-makeCommits(5000);
+// Start the commit process with 1000 commits
+makeCommits(1000);
